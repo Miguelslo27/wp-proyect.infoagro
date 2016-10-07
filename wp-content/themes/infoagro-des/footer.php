ea045167dbc0d62110962bc4f224d76f8dcf6553
Mike <miguel.sosa@globant.com>
2016-10-07 18:10:58 -0300

    Added empty footer
    - Performed styling for footer with wrapper behavior

--- wp-content/themes/infoagro-des/footer.php
+++ wp-content/themes/infoagro-des/footer.php
@@ -1,8 +1,8 @@
-		<footer>
-			<div id="footer">
-				
-			</div> <!-- #footer ends -->
-		</footer>
 	</div> <!-- #wrapper ends -->
+	<footer>
+		<div id="footer">
+			
+		</div> <!-- #footer ends -->
+	</footer> <!-- end footer -->
 </body>
 </html>
