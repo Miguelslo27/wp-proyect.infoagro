02e2a804c7fdb4a606c74bf4f6f0540e4c9338a2
Mike <miguel.sosa@globant.com>
2016-10-14 10:10:11 -0300

    Added Footer content

---
+++ wp-content/themes/infoagro-des/searchform.php
@@ -0,0 +1,8 @@
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<div>
+		<input value="" name="s" id="s" type="text">
+		<button id="searchsubmit" type="submit">
+			<span class="icon ion-search"></span>
+		</button>
+	</div>
+</form>
